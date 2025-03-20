2b586bd1baa87d000adcbc0ce3bafb1621ecaa82
Nasrul Khair <nasrulkhair@yahoo.com>
2025-03-20 13:51:16 +0800

    new file:   final_cleaning.sql         modified:   johor_prop.pbix

---
+++ Project 8 - Johor Property/final_cleaning.sql
@@ -0,0 +1,15 @@
+-- Analysis on Johor Property Dataset --
+
+/* SInce the analysis will only be focusing on the 
+Landed vs high_rise house, the dataset need to be cleaned
+and only have the wanted data only
+*/
+
+
+-- Creating a view table for Power BI import
+
+CREATE VIEW power_bi_import AS
+SELECT *
+FROM johor_prop
+WHERE property_type NOT IN ('shop_lot')
+
