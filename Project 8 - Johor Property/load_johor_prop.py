12e437d37d12024b2233ebd47639d864c15df86f
Nasrul Khair <nasrulkhair@yahoo.com>
2025-03-20 08:29:07 +0800

    modified:   extract_johor_prop.py         modified:   load_johor_prop.py         modified:   transform_johor_prop.ipynb

--- Project 8 - Johor Property/load_johor_prop.py
+++ Project 8 - Johor Property/load_johor_prop.py
@@ -1,14 +1,28 @@
 """-------------------------Load: VSCODE --> MSSQL-----------------------------------"""
 
 
-
 from sqlalchemy import create_engine
 import pandas as pd
 
 
 def load_file(csv):
-    csv_file = csv
-    df = pd.read_csv(csv_file)
+    df = pd.read_csv(csv, index_col=0)
     return df
 
-load_file("C:\Users\User\Desktop\Data Analyst\End To End Project\Project 8 - Johor Property\Transform - Johor_prop.csv")
+# Load data
+
+df = load_file(r"C:\Users\User\Desktop\Data Analyst\End To End Project\Project 8 - Johor Property\transform_johor_prop.csv")
+#print(df.head())
+
+# Connect to MSSQL
+
+server = "NASRULKHAIR\SQLEXPRESS"
+database = "Johor Properties"
+
+conn_str = f"mssql+pyodbc://@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes"
+engine = create_engine(conn_str)
+
+# Load into MSSQL
+df.to_sql("johor_prop", con=engine, if_exists="append", index=False)
+
+print("Data loaded into MSSQL")
