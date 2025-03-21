b66279951ec10debe6a2a970342cef4443006784
Nasrul Khair <nasrulkhair@yahoo.com>
2025-03-22 00:33:42 +0800

    new file:   ab_testing.py         modified:   transform_johor_prop.csv

---
+++ Project 8 - Johor Property/ab_testing.py
@@ -0,0 +1,6 @@
+
+import pandas as pd
+import scipy.stats as stats
+
+df = pd.read_csv(r"C:\Users\User\Desktop\Data Analyst\End To End Project\Project 8 - Johor Property\transform_johor_prop.csv")
+print(df.head())
