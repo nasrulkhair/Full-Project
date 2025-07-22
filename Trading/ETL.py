157e4e4179628687e3624eb6bd0fd9c3342a2134
Nasrul Khair <nasrulkhair@yahoo.com>
2025-07-23 02:07:53 +0800

    modified:   ETL.py

--- Trading/ETL.py
+++ Trading/ETL.py
@@ -1,3 +1,16 @@
 
 
+'''
+ETL Process for Trading Data
+This script outlines the ETL (Extract, Transform, Load) process for trading data.
+It includes the extraction of data from various sources, transformation of the data to fit the analysis needs, and loading the final datasets for further analysis.
+'''
+
+# Required Libraries
+import pandas as pd
+import numpy as np
+import os
+from sqlalchemy import create_engine, pyodbc
+
+
 # 1. Extract Data
