55c5c564efe6771596790b432871d65a4bc93a62
Nasrul Khair <nasrulkhair@yahoo.com>
2025-08-11 11:03:20 +0800

    modified:   PPNJ/docs/6.Query Analysis.docx         new file:   PPNJ/docs/pdf copy/6.Query Analysis.pdf         new file:   PPNJ/docs/pdf copy/7.Data Dictionary.pdf         new file:   PPNJ/docs/pdf copy/8. EDA.pdf         modified:   Trading/ETL.py         new file:   Trading/transformation.ipynb

--- Trading/ETL.py
+++ Trading/ETL.py
@@ -10,7 +10,13 @@ It includes the extraction of data from various sources, transformation of the d
 import pandas as pd
 import numpy as np
 import os
-from sqlalchemy import create_engine, pyodbc
+from sqlalchemy import create_engine
+import pyodbc
 
 
-# 1. Extract Data
+# 1. Extract Data From Local Files
+
+engine = create_engine("mssql+pyodbc://@nasrulkhair\SQLEXPRESS/Data Warehouse?driver=ODBC+Driver+17+for+SQL+Server;Trusted_Connection=yes")
+
+
+# Esok determine the direction of the ETL process. malam ni tidur dulu. kau un blur nak code apa ni
